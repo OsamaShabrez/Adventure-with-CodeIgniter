416af2ec7bd65db70e655962878f151b4ea705e6
OsamaShabrez <osamashabrez@live.com>
2012-09-02 02:32:53 +0500

    updated contact us

--- application/controllers/staticpages.php
+++ application/controllers/staticpages.php
@@ -9,13 +9,24 @@ class StaticPages extends CI_Controller {
   }
 
   public function processContactForm() {
-      
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('name',    'Name',    'trim|required|min_length[5]|max_length[70]|xss_clean');
+    $this->form_validation->set_rules('email',   'Email',   'required|valid_email');
+    $this->form_validation->set_rules('title',   'Title',   'xss_clean');
+    $this->form_validation->set_rules('message', 'Message', 'trim|required|min_length[5]|max_length[70]|xss_clean');
+
+    if( $this->form_validation->run() === false ) {
+        $this->showContactForm();
+    } else {
+        $this->showContactForm();
+    }
   }
 
   public function showContactForm() {
     $data['title'] = 'Contact Us';
 
     $this->load->library('form_validation');
+    
     $this->load->helper('url');
     $data['categories'] = $this->db_model->get_category();
     $data['loggedin'] = $this->session->getLoggedIn();
--- application/views/page/contactus-template.php
+++ application/views/page/contactus-template.php
@@ -12,30 +12,31 @@
         </div>
         <div class="center_prod_box_big">
             <div class="contact_form">
+            <?php echo validation_errors('<li>','</li>'); ?>
                 <?php echo form_open('page/processcontactform'); ?>
                     <div class="form_row">
                         <label style="text-align: center; width: inherit;"></label>
                         <label class="contact"><strong><span>*</span></strong>Name :</label>
-                        <input type="text" maxlength="25" value="" name="names" class="contact_input">
+                        <input type="text" maxlength="25" value="<?php echo set_value('name'); ?>" name="name" class="contact_input">
                     </div>
                     <div class="form_row">
                         <label class="contact"><strong><span>*</span></strong>Email :</label>
-                        <input type="text" maxlength="25" name="email" class="contact_input">
+                        <input type="text" maxlength="25" name="email" value="<?php echo set_value('email'); ?>" class="contact_input">
                     </div>
                     <div class="form_row">
                         <label class="contact">Title :</label>
-                        <input type="text" maxlength="25" name="email" class="contact_input">
+                        <input type="text" maxlength="25" name="title" value="<?php echo set_value('title'); ?>" class="contact_input">
                     </div>
                     <div class="form_row">
                         <label class="contact"><strong><span>*</span></strong>Message :</label>
-                        <textarea name="message" class="contact_textarea"></textarea>
+                        <textarea name="message" class="contact_textarea"><?php echo set_value('title'); ?></textarea>
                     </div>
                     <div class="form_row">
                             <input type="submit" value="Send!" class="button"><br>
                     </div>      
                 </form>
-                <p id="error"><?php echo validation_errors(); ?></p>
             </div> 
+            <div style="clear:both;"></div>
         </div>
         <div class="bottom_prod_box_big"></div>
     </div>
